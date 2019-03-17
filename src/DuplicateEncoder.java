36ac6b55a3649ae12217bc61c1037e5b758a8559
Aleksej Bratkovskij <alexeybrat@gmail.com>
2019-03-17 18:11:53 +0000

    Made first test pass, bracket gives me headacke.

--- src/DuplicateEncoder.java
+++ src/DuplicateEncoder.java
@@ -5,7 +5,7 @@
  Ignore capitalization when determining if a character is a duplicate.
  */
 //TODO: Make second test pass.
- class DuplicateEncoder {
+class DuplicateEncoder {
     static String encode(String word) {
         word = word.toLowerCase();
         for(char c : word.toCharArray()) {
@@ -15,7 +15,6 @@
                 else word = word.replace(c, ')');
             }
         }
-
         return word;
     }
 }
