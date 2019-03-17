38d634eb9f352e2f56bc2f062fb8f8504e886f11
Aleksej Bratkovskij <alexeybrat@gmail.com>
2019-03-17 22:05:45 +0000

    Made first test pass, bracket gives me headacke.

--- src/DuplicateEncoder.java
+++ src/DuplicateEncoder.java
@@ -4,11 +4,12 @@
  in the original string, or ")" if that character appears more than once in the original string.
  Ignore capitalization when determining if a character is a duplicate.
  */
-//TODO: Make second test pass.
+
 class DuplicateEncoder {
     static String encode(String word) {
         word = word.toLowerCase();
         for(char c : word.toCharArray()) {
+            //TODO: place check for ) and ( here.
             if (Character.isLetterOrDigit(c)) {
                 if (word.indexOf(c) == word.lastIndexOf(c) && c != ')' && c != '(')
                     word = word.replace(c, '(');
